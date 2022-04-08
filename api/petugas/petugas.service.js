5bad1afdf6292406c3a9f9edfe454e7d8c97baab
captvin <alpinoke82@gmail.com>
2022-04-08 08:04:14 +0700

    add file

---
+++ api/petugas/petugas.service.js
@@ -0,0 +1,45 @@
+const model = require('../../models/Petugas')
+
+module.exports = {
+
+    add: function (data, callback) {
+        model.create(data)
+            .then(result => callback(null, result))
+            .catch(error => callback(error))
+    }
+    ,
+
+    get: function (callback) {
+        model.findAll()
+            .then(result => callback(null, result))
+            .catch(error => callback(error))
+    }
+    ,
+
+    get_by_id: function (id, callback) {
+        model.findByPk(id)
+            .then(result => callback(null, result))
+            .catch(error => callback(error))
+    }
+    ,
+
+    update: function (data, callback) {
+        model.update(data, { where: { id: data.id } })
+            .then(result => callback(null, result))
+            .catch(error => callback(error))
+    }
+    ,
+
+    del: function (id, callback) {
+        model.destroy({ where: { id }, returning: true })
+            .then(result => callback(null, result))
+            .catch(error => callback(error))
+    }
+    ,
+
+    get_user_by_email: function (email, callback) {
+        model.findOne({ where: { email } })
+            .then(result => callback(null, result))
+            .catch(error => callback(error))
+    }
+}

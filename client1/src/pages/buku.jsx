6e6c384ddb18de0f59c17212dd1d2641152f039b
captvin <alpinoke82@gmail.com>
2022-04-26 17:06:12 +0700

    fix page buku

--- client1/src/pages/buku.jsx
+++ client1/src/pages/buku.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import axios from 'axios'
 import { Link, NavLink } from 'react-router-dom'
 import { Footer, Navbar } from '../components'

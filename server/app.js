633746e65f2f10646f1ffa3bc77207b8783d3be4
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-05-09 16:13:57 +0700

    installing cors for server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors({ origin: '*', credentials: true }))
 
 const router = require('./router')
 
